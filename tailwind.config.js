27df9a9f242d7d618f739d9876b1d04d5b5db3c0
tompra <tomprayon@gmail.com>
2024-01-17 20:51:42 +0100

    add animation infinite scroll

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,17 @@
 export default {
     content: ['./index.html', './src/**/*.{js,ts,jsx,tsx}'],
     theme: {
-        extend: {},
+        extend: {
+            animation: {
+                'infinite-scroll': 'infinite-scroll 25s linear infinite',
+            },
+            keyframes: {
+                'infinite-scroll': {
+                    '0%': { transform: 'translateX(0)' },
+                    '100%': { transform: 'translateX(-100%)' },
+                },
+            },
+        },
     },
     plugins: [],
 };
