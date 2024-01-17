75c54bab8b0aabb84a4d12ae1195ff1db7e78a87
tompra <tomprayon@gmail.com>
2024-01-17 14:42:28 +0100

    add hero component

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,11 @@
 import Navbar from './components/Navbar';
+import Hero from './components/Hero';
 
 const App = () => {
     return (
         <div className='min-h-screen bg-emerald-600 px-10'>
             <Navbar />
+            <Hero />
         </div>
     );
 };
--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,30 +1,29 @@
 import { BsFillMoonStarsFill } from 'react-icons/bs';
-
 import { links } from '../data';
 
 const Navbar = () => {
     return (
-        <nav className='bg-gradient-to-r from-cyan-500 to-teal-500 py-10 mb-12'>
+        <nav className='bg-gradient-to-r from-cyan-500 to-teal-500 py-10'>
             <div className='mx-auto max-w-7xl px-8 py-4 flex flex-col sm:flex-row sm:gap-x-16 sm:items-center sm:py-8'>
-                <h1 className='text-xl text-white inline-block'>TP</h1>
-                <ul className='flex gap-x-3'>
+                <h1 className='text-xl text-white inline-block sm:mb-4 sm:mr-8'>
+                    TP
+                </h1>
+                <ul className='flex flex-col sm:flex-row sm:gap-x-3'>
                     {links.map((link) => {
                         const { id, href, text } = link;
                         return (
-                            <>
-                                <li>
-                                    <a
-                                        key={id}
-                                        href={href}
-                                        className='capitalize text-lg tracking-wider hover:text-white duration-300'
-                                    >
-                                        {text}
-                                    </a>
-                                </li>
-                            </>
+                            <li key={id}>
+                                <a
+                                    key={id}
+                                    href={href}
+                                    className='capitalize text-lg tracking-wider hover:text-white duration-300'
+                                >
+                                    {text}
+                                </a>
+                            </li>
                         );
                     })}
-                    <li className='px-4 '>
+                    <li className='md:ml-auto'>
                         <BsFillMoonStarsFill className='cursor-pointer text-2xl' />
                     </li>
                 </ul>
