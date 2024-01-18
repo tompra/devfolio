5106146794cc438d63a32063cf48fac948fc4599
tompra <tomprayon@gmail.com>
2024-01-18 10:20:35 +0100

    Add projects component

---
+++ src/components/Projects.jsx
@@ -0,0 +1,8 @@
+const Projects = () => {
+    return (
+        <section className=' bg-pink-600 align-element '>
+            <h1 className='text-3xl'>Proyects</h1>
+        </section>
+    );
+};
+export default Projects;
