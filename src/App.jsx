7271120002a7b7240e37859b3f64602bf5458ad7
tompra <tomprayon@gmail.com>
2024-01-18 10:06:42 +0100

    add about component

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,14 @@
 import Navbar from './components/Navbar';
 import Hero from './components/Hero';
 import Skills from './components/Skills';
+import About from './components/About';
 
 const App = () => {
     return (
         <div className='min-h-screen bg-emerald-600 px-10'>
             <Navbar />
             <Hero />
+            <About />
             <Skills />
         </div>
     );
---
+++ src/components/About.jsx
@@ -0,0 +1,31 @@
+const About = () => {
+    return (
+        <section className=" bg-rose-400 align-element py-5'">
+            <div>
+                <h1 className='text-3xl'>About me</h1>
+                <p>
+                    Lorem, ipsum dolor sit amet consectetur adipisicing elit.
+                    Dolorum porro illum non consequuntur iusto, id doloribus
+                    iste, atque maxime blanditiis minima exercitationem, vitae
+                    cupiditate quod. Pariatur aut dolorum veniam blanditiis!
+                </p>
+                <p>
+                    Lorem ipsum dolor sit amet consectetur adipisicing elit.
+                    Eaque praesentium molestiae, dolore odit quae amet autem ex
+                    facere voluptates! Autem voluptate expedita iste, adipisci
+                    dolor maiores unde tempora! Magnam, accusantium.
+                </p>
+                <p>
+                    Lorem ipsum dolor sit amet consectetur adipisicing elit.
+                    Eaque praesentium molestiae, dolore odit quae amet autem ex
+                    facere voluptates! Autem voluptate expedita iste, adipisci
+                    dolor maiores unde tempora! Magnam, accusantium.
+                </p>
+            </div>
+            <div>
+                <img src='https://placehold.co/300x400' />
+            </div>
+        </section>
+    );
+};
+export default About;
