f6f54108941329b985d886771e40e57231f33db6
tompra <tomprayon@gmail.com>
2024-02-02 20:56:09 +0100

    change layout btns in mobile

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -70,20 +70,20 @@ const About = () => {
                             className='object-cover w-full h-full rounded-lg hover:opacity-95'
                         />
                     </div>
-                    <div className='flex justify-center  pt-10'>
-                        <button
+                    <div className='xs:flex xs:flex-col xs:items-center xs:gap-y-4 sm:flex sm:justify-center  xs:py-5 sm:py-10'>
+                        <a
                             onClick={downloadCV}
-                            className=' relative inline-flex items-center justify-center p-0.5 mb-2 me-2 overflow-hidden text-sm font-medium text-gray-900 rounded-lg group bg-gradient-to-br from-purple-600 to-blue-500 group-hover:from-purple-600 group-hover:to-blue-500 hover:text-white dark:text-white focus:ring-4 focus:outline-none focus:ring-blue-300 dark:focus:ring-blue-800'
+                            className='cursor-pointer relative inline-flex items-center justify-center p-0.5 mb-2 me-3 overflow-hidden text-sm font-medium text-gray-900 rounded-lg group bg-gradient-to-br from-purple-600 to-blue-500 group-hover:from-purple-600 group-hover:to-blue-500 hover:text-white dark:text-white focus:ring-4 focus:outline-none focus:ring-blue-300 dark:focus:ring-blue-800'
                         >
-                            <span className='xs:text-xs sm:text-base relative px-4 text-center xs:py-3.5 sm:py-2.5  transition-all ease-in duration-75  bg-white dark:bg-gray-900 rounded-md group-hover:bg-opacity-0'>
+                            <span className=' relative px-4 text-center py-2.5 transition-all ease-in duration-75  bg-white dark:bg-gray-900 rounded-md group-hover:bg-opacity-0'>
                                 Download CV
                             </span>
-                        </button>
+                        </a>
                         <a
                             href='#projects'
-                            className='  relative inline-flex items-center justify-center p-0.5 mb-2 me-3 overflow-hidden text-sm font-medium text-gray-900 rounded-lg group bg-gradient-to-br from-purple-600 to-blue-500 group-hover:from-purple-600 group-hover:to-blue-500 hover:text-white dark:text-white focus:ring-4 focus:outline-none focus:ring-blue-300 dark:focus:ring-blue-800'
+                            className='cursor-pointer relative inline-flex items-center justify-center p-0.5 mb-2 me-3 overflow-hidden text-sm font-medium text-gray-900 rounded-lg group bg-gradient-to-br from-purple-600 to-blue-500 group-hover:from-purple-600 group-hover:to-blue-500 hover:text-white dark:text-white focus:ring-4 focus:outline-none focus:ring-blue-300 dark:focus:ring-blue-800'
                         >
-                            <span className='xs:text-xs sm:text-base relative px-4 text-center xs:py-3.5 sm:py-2.5 transition-all ease-in duration-75  bg-white dark:bg-gray-900 rounded-md group-hover:bg-opacity-0'>
+                            <span className=' relative px-4 text-center py-2.5 transition-all ease-in duration-75  bg-white dark:bg-gray-900 rounded-md group-hover:bg-opacity-0'>
                                 Check my work
                             </span>
                         </a>
